89d3526be4a9bdc6c514b1e62d2a954295178698
Wubsera <wubsera21@gmail.com>
2022-07-03 15:37:06 -0700

    commited

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,9 +8,12 @@
 
 int main(void)
 {
+
 	int n;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	/*here is my code*/
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
@@ -18,4 +21,5 @@ int main(void)
 	else
 	printf("%d is negative\n", n);
 	return (0);
+	/*return*/
 }
