a37dda055990998c724d304d14b12f0bf262f58e
Wubsera <wubsera21@gmail.com>
2022-07-04 08:59:07 -0700

    alphabet commited

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - the main function
+ *
+ * Description: print alphabet
+ *
+ * Return: return true or false
+ */
+int main(void)
+{
+	int low;
+
+	for (low ='a'; low <= 'z'; low++)
+	{
+		putchar(low);
+	}
+	purchar(\n);
+	return (0);
+}
