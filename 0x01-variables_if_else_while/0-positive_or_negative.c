1ff9283700b9fedae649b406aa772eba08347bab
Wubsera <wubsera21@gmail.com>
2022-07-04 02:53:41 -0700

    chnage ommited

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,9 @@
 #include <time.h>
 
 /**
- *This is my first code in c language. This program checks if the number is positive or negative 
- */
+*This is my first code in c language.
+*This program checks if the number is positive or negative 
+*/
 int main(void)
 {
 	int n;
