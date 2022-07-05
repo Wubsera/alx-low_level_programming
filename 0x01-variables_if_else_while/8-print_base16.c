bffdfa2a52f83f7dcb8a20ad80dcfc8afb3ba338
Wubsera <wubsera21@gmail.com>
2022-07-04 21:22:53 -0700

    8-print_base16.c commited

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - the main function
+ *
+ * Description: this is my first c program in vi
+ *
+ * Return: return true or false
+ */
+int main(void)
+{
+	char low;
+	char row;
+
+	for (low = 0; low < 10; low++)
+	{
+		putchar(low + '0');
+	}
+	for (low = 10; low <=15; low++)
+	{
+		putchar(row);
+		row++;
+	}
+	putchar('\n');
+
+	return (0);
+}
