49c30339f5ac8a3230c4c36ff2ab68b2617ae7ca
Wubsera <wubsera21@gmail.com>
2022-07-04 20:58:39 -0700

    5-print_numbers.c commited

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - the main function
+ *
+ * Description: this is my first c program in vi
+ *
+ * Return: return true or false
+ */
+int main(void)
+{
+	int num;
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+	putchar('\n');
+	return (0);
+}
