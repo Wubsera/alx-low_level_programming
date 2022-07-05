9432c65033452e1a75c7ee651d2e02d8af269302
Wubsera <wubsera21@gmail.com>
2022-07-04 20:51:19 -0700

    4-print_alphabt.c commited

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - the main function
+ *
+ * Description: this is my first c program in vi
+ *
+ * Return: return true or false
+ */
+int main(void)
+{
+	char low;
+	low = 'a';
+	while (low <= 'z')
+	{
+		if (low != 'e' && low != 'q')
+		{
+			putchar(low);
+		}
+		low++;
+	}
+	putchar('\n');
+	return (0);
+}
