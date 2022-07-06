e740adea3aec3603b23faefa3f0896f56bb53e62
Wubsera <wubsera21@gmail.com>
2022-07-06 14:18:54 -0700

    11-print_to_98.c commited

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -25,4 +25,16 @@ void print_to_98(int n)
 		}
 		printf("\n");
 	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i > 98)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
 }
