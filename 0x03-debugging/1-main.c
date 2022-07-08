2d74cf57f558398ee0fa3748304cbc8a0a8cbc60
Wubsera <wubsera21@gmail.com>
2022-07-08 00:15:18 -0700

    1-main.c commited

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,28 +2,23 @@
 
 /**
  * main - causes an infinite loop
- *
- * Description: Describe the main funtion
- *
  * Return: 0
  */
 
 int main(void)
 {
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
-	
 	i = 0;
-	
+
 	/**
 	 * while (i < 10)
 	 * {
 	 * 	putchar(i);
 	 * }
 	 */
-	
+
 	printf("Infinite loop avoided! \\o/\n");
-	
 	return (0);
 }
