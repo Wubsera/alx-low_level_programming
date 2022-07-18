1beff191b9cb5ad1156283d9817eea723c87f57c
Wubsera <wubsera21@gmail.com>
2022-07-18 04:18:31 -0700

    5-strstr.c commited

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,16 +14,17 @@ char *_strstr(char *haystack, char *needle)
 {
 	while (*haystack)
 	{
-		char *temp;
 
-		temp = haystack;
-		while (*needle == *haystack)
-		{
-			haystack++, needle++;
-		}
-		if (*needle == '\0')
-			return (temp);
-		haystack++;
+	char *temp;
+
+	temp = haystack;
+	while (*needle == *haystack)
+	{
+		haystack++, needle++;
+	}
+	if (*needle == '\0')
+		return (temp);
+	haystack++;
 	}
 	return (0);
 }
