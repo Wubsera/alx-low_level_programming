bcc15bdeb15c77ea219c2bb3a30867efb24da254
Wubsera <wubsera21@gmail.com>
2022-08-01 08:02:17 -0700

    4-free is commited

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,16 +1,38 @@
 #include "dog.h"
-#include <stdlib.h>
 /**
- *  * free_dog - frees the dogs..
- *   * @d: Pointer to the struct.
- *    * Return: None.
+ *   * new_dog - creates a new dog
+ *     * @name: new dog's name
+ *       * @age: new dog's age
+ *         * @owner: new dog's owner
+ *           * Return: pointer to new dog struct
  */
-void free_dog(dog_t *d)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d)
+	dog_t *new_dog;
+	int i;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = malloc(sizeof(name) + 1);
+	if (new_dog->name == NULL)
 	{
-		free(d->name);
-		free(d->owner);
-		free(d);
+		free(new_dog);
+		return (NULL);
 	}
+	new_dog->owner = malloc(sizeof(owner) + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	for (i = 0; name[i]; i++)
+		new_dog->name[i] = name[i];
+	new_dog->name[i] = '\0';
+	new_dog->age = age;
+	for (i = 0; owner[i]; i++)
+		new_dog->owner[i] = owner[i];
+	new_dog->owner[i] = '\0';
+	return (new_dog);
 }
