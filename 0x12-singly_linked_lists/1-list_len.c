c67da12c14153f3b02a6a7038710796e1e2dfb51
Wubsera <wubsera21@gmail.com>
2022-08-11 12:42:02 -0700

    commited

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,9 @@
 #include "lists.h"
 /**
- *  * list_len - function that returns the number of elements in a linked list_t
- *   *            list.
- *    *
- *     * @h: This is the sigle list
- *      *
- *       * Return: The number of elements in a linked list_t list
+ * list_len - function that returns the number of elements in a linked list_t
+ * list.
+ * @h: This is the sigle list
+ * Return: The number of elements in a linked list_t list
  */
 size_t list_len(const list_t *h)
 {
