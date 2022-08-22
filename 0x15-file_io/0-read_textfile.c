4a766eb85b5585901061ace7617f40486d081cd9
Wubsera <wubsera21@gmail.com>
2022-08-22 02:40:21 -0700

    o-read ... commited

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,33 +1,38 @@
 #include "main.h"
 /**
- *  * read_textfile - reads a text file and prints it to the POSIX standard output
- *   * @filename: The name of the file to open it
- *    * @letters: The numbers of bytes to read
- *     * Return: Number of bytes to read and write to the output
+ *  * read_textfile - read a textfile.
+ *   * @filename: name of the file to open.
+ *    * @letters: number of letters to print in the standar ouput.
+ *     *
+ *      * Return: number of letter printed.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char *buf;
-	int lenRead, lenWrite;
+	int fd_read, count_chars, fd_open;
+	char *buf_letters;
 
 	if (filename == NULL)
 		return (0);
-
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	fd_open = open(filename, O_RDONLY);
+	if (fd_open == -1)
 		return (0);
-	buf = malloc(letters * sizeof(char));
-
-	if (buf == NULL)
+	buf_letters = malloc(sizeof(char) * letters);
+	if (buf_letters == NULL)
 		return (0);
-
-	lenRead = read(fd, buf, letters);
-
-	lenWrite = write(STDOUT_FILENO, buf, lenRead);
-	if (lenWrite != lenRead && lenWrite == -1)
+	fd_read = read(fd_open, buf_letters, letters);
+	if (fd_read == -1)
+	{
+		free(buf_letters);
 		return (0);
-	free(buf);
-	close(fd);
-	return (lenRead);
+	}
+	count_chars = write(STDOUT_FILENO, buf_letters, fd_read);
+	if (count_chars == -1)
+	{
+		free(buf_letters);
+		return (0);
+	}
+	close(fd_open);
+	free(buf_letters);
+	return (count_chars);
 }
