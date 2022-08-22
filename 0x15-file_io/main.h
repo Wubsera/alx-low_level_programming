4a491f90ad5764d0fc246c795e4c90e8dfd00a93
Wubsera <wubsera21@gmail.com>
2022-08-22 02:43:57 -0700

    .

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,23 +1,15 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#define BUF_SIZE 1024
+
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-#endif#ifndef HEADER_FILE
-#define HEADER_FILE
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
+int _strlen(char *str);
+void close_file(int FD);
 #endif
