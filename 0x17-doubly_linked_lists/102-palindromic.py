df8e37d6c5ad3e3723986c6b61792e5b89ad3b2f
Wubsera <wubsera21@gmail.com>
2022-09-29 22:19:20 -0700

    102-palindromic.py

--- 0x17-doubly_linked_lists/102-palindromic.py
+++ 0x17-doubly_linked_lists/102-palindromic.py
@@ -4,7 +4,7 @@ def isPali(str):
         if str[i] != str[-1 - i]:
             return False
     return True
-    
+
     for i in range(999*999, -1, -1):
         if isPali(str(i)):
             print("Found: ", i)
